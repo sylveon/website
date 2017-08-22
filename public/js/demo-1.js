0c3f65acb8f20237787fd653c076ead57f2ca52f
Charles Milette <charles.milette@gmail.com>
2017-08-22 16:04:25 -0400

    Rename canvas

--- public/js/demo-1.js
+++ public/js/demo-1.js
@@ -15,7 +15,7 @@
         largeHeader = document.getElementById('large-header');
         largeHeader.style.height = height+'px';
 
-        canvas = document.getElementById('demo-canvas');
+        canvas = document.getElementById('canvas');
         canvas.width = width;
         canvas.height = height;
         ctx = canvas.getContext('2d');
@@ -181,5 +181,5 @@
     function getDistance(p1, p2) {
         return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
     }
-    
+
 })();
