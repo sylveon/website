ce8c8fc9d0a769bc0e6e3e502d7508bfa85183b4
Charles Milette <charles.milette@gmail.com>
2017-08-26 20:10:14 -0400

    Remove loading things

--- public/terminology-themes/js/main.js
+++ public/terminology-themes/js/main.js
@@ -13,13 +13,6 @@ $(function () {
 		xsmall: '(max-width: 480px)'
 	});
 
-	// Disable animations/transitions until everything's loaded.
-	$body.addClass('is-loading');
-
-	$window.on('load', function () {
-		$body.removeClass('is-loading');
-	});
-
 	// Poptrox.
 	$window.on('load', function () {
 
