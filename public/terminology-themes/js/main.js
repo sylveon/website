0b90e07b5427924c34c52bd2486d9c15b32f77af
Charles Milette <charles.milette@gmail.com>
2017-08-28 16:21:49 -0400

    De-jQuery-fy things

--- public/terminology-themes/js/main.js
+++ public/terminology-themes/js/main.js
@@ -1,9 +1,5 @@
 $(function () {
 
-	// Vars.
-	var $window = $(window),
-		$body = $('body');
-
 	// Breakpoints.
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
@@ -14,14 +10,14 @@ $(function () {
 	});
 
 	// Poptrox.
-	$window.on('load', function () {
+	window.onload = function () {
 
 		$('.thumbnails').poptrox({
 			onPopupClose: function () {
-				$body.removeClass('is-covered');
+				document.body.classList.remove("is-covered");
 			},
 			onPopupOpen: function () {
-				$body.addClass('is-covered');
+				document.body.classList.add('is-covered');
 			},
 			baseZIndex: 10001,
 			useBodyOverflow: false,
@@ -34,12 +30,13 @@ $(function () {
 			windowMargin: (skel.breakpoint('small').active ? 5 : 50)
 		});
 
-	});
+	};
 
 	var themesArray = ["3024Day", "3024Night", "AdventureTime", "Afterglow", "AlienBlood", "Argonaut", "Arthur", "AtelierSulphurpool", "Atom", "AtomOneLight", "ayu", "ayu_light", "Batman", "BelafonteDay", "BelafonteNight", "BirdsOfParadise", "Blazer", "BlulocoDark", "BlulocoLight", "Borland", "BrightLights", "Broadcast", "Brogrammer", "C64", "Chalkboard", "Chalk", "Ciapre", "CLRS", "Cobalt2", "CobaltNeon", "CrayonPonyFish", "DarkPastel", "Darkside", "deep", "Desert", "DimmedMonokai", "DotGov", "Dracula", "DuotoneDark", "Earthsong", "Elemental", "Elementary", "ENCOM", "Espresso", "EspressoLibre", "Fideloper", "FirefoxDev", "Firewatch", "FishTank", "Flat", "Flatland", "Floraverse", "ForestBlue", "FrontEndDelight", "FunForrest", "Galaxy", "Github", "Glacier", "Grape", "Grass", "GruvboxDark", "Hacktober", "Hardcore", "Harper", "Highway", "HipsterGreen", "Homebrew", "Hurtado", "Hybrid", "IC_Green_PPL", "IC_Orange_PPL", "idleToes", "IR_Black", "JackieBrown", "Japanesque", "Jellybeans", "JetBrainsDarcula", "Kibble", "LaterThisEvening", "Lavandula", "LiquidCarbon", "LiquidCarbonTransparent", "LiquidCarbonTransparentInverse", "ManPage", "MaterialDark", "Material", "Mathias", "Medallion", "Misterioso", "Molokai", "MonaLisa", "MonokaiSoda", "MonokaiVivid", "N0tch2k", "Neopolitan", "Neutron", "NightLionv1", "NightLionv2", "Novel", "Obsidian", "Ocean", "OceanicMaterial", "Ollie", "OneHalfDark", "OneHalfLight", "Pandora", "ParaisoDark", "ParasioDark", "PaulMillr", "PencilDark", "PencilLight", "PiattoLight", "Pnevma", "Pro", "ProLight", "RedAlert", "RedPlanet", "RedSands", "Rippedcasts", "Royal", "Ryuuko", "SeafoamPastel", "SeaShells", "Seti", "Shaman", "Slate", "Smyck", "Snazzy", "SoftServer", "SolarizedDarcula", "SolarizedDark", "SolarizedDarkHigherContrast", "SolarizedDark-Patched", "SolarizedLight", "Spacedust", "SpaceGray", "SpaceGrayEightiesDull", "SpaceGrayEighties", "Spiderman", "Spring", "Square", "Sundried", "Symfonic", "synthwave", "TangoAdapted", "TangoHalfAdapted", "Teerb", "TerminalBasic", "ThayerBright", "TheHulk", "Tomorrow", "TomorrowNightBlue", "TomorrowNightBright", "TomorrowNight", "TomorrowNightEighties", "ToyChest", "Treehouse", "Twilight", "Ubuntu", "UnderTheSea", "Urple", "Vaughn", "VibrantInk", "VioletDark", "VioletLight", "WarmNeon", "Wez", "WildCherry", "Wombat", "Wryan", "Zenburn"];
 	var columnToAdd = 1;
 
 	document.getElementById("total-number").textContent = themesArray.length;
+	themesArray.sort();
 
 	// Auto-gen
 	themesArray.forEach(function (theme) {
