7780ad2614be5e1c05d15aa6fc818e8043674ab2
Charles Milette <charles.milette@gmail.com>
2017-10-11 13:55:35 -0400

    Fix error if no files in public changed or a file was added

--- delete-changed-files.sh
+++ delete-changed-files.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-FILES_TO_DELETE=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep public/ | sed "s/public\//https:\/\/charlesmilette.net\//g" | head -c -1 | jq -R -s -c "split(\"\n\")")
+FILES_TO_DELETE=$(git diff --name-only --diff-filter=a ${TRAVIS_COMMIT_RANGE} | grep public/ | sed "s/public\//https:\/\/charlesmilette.net\//g" | head -c -1 | jq -R -s -c "split(\"\n\")")
 
-SUCCESS=$(curl --silent -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" -H "X-Auth-Key: ${CLOUDFLARE_API_TOKEN}" -H "Content-Type: application/json" --data "{\"files\":${JSON_ARRAY_FILES_TO_DELETE}}" | jq -r .success)
+if [[ "${PIPESTATUS[1]}" != "0" ]]
+then
+    exit 0
+fi
+
+SUCCESS=$(curl --silent -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" -H "X-Auth-Key: ${CLOUDFLARE_API_TOKEN}" -H "Content-Type: application/json" --data "{\"files\":${FILES_TO_DELETE}}" | jq -r .success)
 
 
 
-if [[ "$SUCCESS" == "true" ]]
+if [[ "${SUCCESS}" == "true" ]]
 then
     exit 0
 else
