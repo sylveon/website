bfcbd71a699e5f600a16fb75ceae7a5fb2e9b264
Charles Milette <charles.milette@gmail.com>
2017-10-31 18:41:10 -0400

    Update script to use more git built-ins

--- delete-changed-files.sh
+++ delete-changed-files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FILES_TO_DELETE=$(git diff --name-only --diff-filter=a ${TRAVIS_COMMIT_RANGE} | grep public/ | sed "s/public\//https:\/\/charlesmilette.net\//g" | head -c -1 | jq -R -s -c "split(\"\n\")")
+FILES_TO_DELETE=$(git diff --exit-code --name-only --diff-filter=a ${TRAVIS_COMMIT_RANGE} public/ | sed "s/public\//https:\/\/charlesmilette.net\//g" | head -c -1 | jq -R -s -c "split(\"\n\")")
 
-if [[ "${PIPESTATUS[1]}" != "0" ]]
+if [[ "${PIPESTATUS[0]}" != "1" ]]
 then
-    exit 0
+    exit ${PIPESTATUS[0]}
 fi
 
 SUCCESS=$(curl --silent -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" -H "X-Auth-Key: ${CLOUDFLARE_API_TOKEN}" -H "Content-Type: application/json" --data "{\"files\":${FILES_TO_DELETE}}" | jq -r .success)
