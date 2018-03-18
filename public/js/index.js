7ce7ae01a557350b721844225d2fb8ef6ecd6a1c
Charles Milette <charles.milette@gmail.com>
2018-03-18 15:56:11 -0400

    Background fade in, add referral, move env vars to travis.yml file

--- public/js/index.js
+++ public/js/index.js
@@ -18,15 +18,18 @@
 	fetch(`https://api.unsplash.com/photos/random?w=${width}&h=${height}&orientation=${orientation}&client_id=${clientId}`).then(r =>
 		r.json()
 	).then(image => {
-		const body   = document.body;
+		const back   = document.getElementById("back");
 		const source = document.getElementById("source");
 		const author = document.getElementById("author");
 
-		body.style.setProperty("--background", "url('" + image.urls.custom + "')");
-		source.href = image.user.links.html;
-		author.textContent = image.user.name ? image.user.name : image.user.username;
+		back.onload = () => {
+			source.classList.add("loaded");
+			back.classList.add("loaded");
+		};
 
-		source.classList.add("loaded");
+		back.src = image.urls.custom;
+		source.href = image.user.links.html + "?utm_source=charlesmilette-website&utm_medium=referral";
+		author.textContent = image.user.name ? image.user.name : image.user.username;
 	});
 
 })();
