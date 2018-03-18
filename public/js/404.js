ae99e654be80c563717fa2a6da6b019c2d4fbf3c
Charles Milette <charles.milette@gmail.com>
2018-03-18 12:44:45 -0400

    Remove particle background on main page, use CSS for full width instead of JS, use unsplash for background image

--- public/js/404.js
+++ public/js/404.js
@@ -3,38 +3,13 @@
 
 (() => {
 
-	let width, height, largeHeader = true;
-
-	// Main
-	initHeader();
-	addListeners();
-
-	function initHeader() {
-		width = window.innerWidth;
-		height = window.innerHeight;
-
-		largeHeader = document.getElementById("large-header");
-		largeHeader.style.height = height + "px";
-
-		particleground(largeHeader, {
-			dotColor: "#fff",
-			lineColor: "#555555",
-			particleRadius: 6,
-			curveLines: true,
-			density: 9000,
-			proximity: 100
-		});
-	}
-
-	// Event handling
-	function addListeners() {
-		window.addEventListener("resize", resize);
-	}
-
-	function resize() {
-		width = window.innerWidth;
-		height = window.innerHeight;
-		largeHeader.style.height = height + "px";
-	}
+	particleground(document.body, {
+		dotColor: "#fff",
+		lineColor: "#555555",
+		particleRadius: 6,
+		curveLines: true,
+		density: 9000,
+		proximity: 100
+	});
 
 })();
---
+++ put-secrets.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -i "s/%{id}/${UNSPLASH_CLIENT_ID}/g" public/js/index.js
