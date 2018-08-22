fb3a9c35f884b34ff3b2b32ec20faf6df721ffd5
Charles Milette <me@charlesmilette.net>
2018-08-22 18:16:32 -0400

    Oh god why did this even work

--- public/js/index.js
+++ public/js/index.js
@@ -15,16 +15,16 @@
 		orientation = "portrait";
 	}
 
-	const data  = await fetch(`https://api.unsplash.com/photos/random?w=${width}&h=${height}&orientation=${orientation}&client_id=${clientId}`);
-	const image = await data.json();
-	const back  = document.getElementById("back");
+	const image  = await fetch(`https://api.unsplash.com/photos/random?w=${width}&h=${height}&orientation=${orientation}&client_id=${clientId}`).then(r => r.json());
+	const back   = document.getElementById("back");
+	const source = document.getElementById("source");
 	back.onload = () => {
 		source.classList.add("loaded");
 		back.classList.add("loaded");
 	};
 
 	back.src = image.urls.custom;
-	document.getElementById("source").href = image.user.links.html + "?utm_source=charlesmilette-website&utm_medium=referral";
+	source.href = image.user.links.html + "?utm_source=charlesmilette-website&utm_medium=referral";
 	document.getElementById("author").textContent = image.user.name ? image.user.name : image.user.username;
 
 })();
