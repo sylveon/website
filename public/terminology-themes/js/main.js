04599c7713af35927fd2d1f281c1c6f5bba2ac40
Charles Milette <charles.milette@gmail.com>
2020-08-29 00:12:35 -0400

    Perf improvements

--- public/terminology-themes/js/main.js
+++ public/terminology-themes/js/main.js
@@ -1,25 +1,60 @@
 "use strict";
 /* globals LuminousGallery */
 
-(async () => {
+async function asyncInit() {
+	const result = await fetch("https://api.github.com/repos/sylveon/website/contents/public/terminology-themes/img/themes");
+	if (result.ok) {
+		const themes = (await result.json())
+			.map(f => f.name.replace(".png", ""))
+			.sort((a, b) => a.localeCompare(b, { "sensitivity": "base" }))
+			.map(themeName => {
+				const imageFile = `img/themes/${encodeURIComponent(themeName)}.png`;
+
+				const card = document.createElement("a");
+				card.href = imageFile;
+
+				const thumbnail = document.createElement("img");
+				thumbnail.setAttribute("data-src", imageFile);
+				thumbnail.width = 642 * 0.8;
+				thumbnail.height = 390 * 0.8;
+				thumbnail.alt = themeName;
+				thumbnail.className = "lazyload";
+
+				const name = document.createElement("h3");
+				name.textContent = themeName;
+
+				card.append(thumbnail, name);
+
+				const item = document.createElement("li");
+				item.appendChild(card);
+				document.getElementById("thumblist").appendChild(item);
+
+				return card;
+			});
+
+		document.getElementById("total-number").textContent = themes.length;
+
+		new LuminousGallery(themes, {}, {
+			caption: e => e.textContent,
+			closeOnScroll: true
+		});
+
+		document.body.classList.add("loaded");
+	}
+}
+
+function hideStuff(distros, instructions) {
+	distros.forEach(d => d.classList.remove("active"));
+	instructions.forEach(i => i.style.display = "none");
+}
+
+(() => {
+	asyncInit();
+
 	const distros = [...document.getElementById("distros").children];
 	const instructions = [...document.getElementById("instructions").children];
 
 	hideStuff(distros, instructions);
-
-	const themes = await fetch("https://api.github.com/repos/sylveon/website/contents/public/terminology-themes/img/themes")
-		.then(r => r.json())
-		.then(d => d
-			.map(f => f.name.replace(".png", ""))
-			.sort((a, b) => a.localeCompare(b, { "sensitivity": "base" }))
-			.map(renderTheme));
-
-	document.getElementById("total-number").textContent = themes.length;
-
-	new LuminousGallery(themes, {}, {
-		caption: e => e.textContent,
-		closeOnScroll: true
-	});
 
 	distros.forEach((e, i) => e.onclick = () => {
 		if (e.classList.contains("active")) {
@@ -30,36 +65,4 @@
 			instructions[i].style.display = "";
 		}
 	});
-
-	document.body.classList.add("loaded");
 })();
-
-function renderTheme(themeName) {
-	const imageFile = `img/themes/${encodeURIComponent(themeName)}.png`;
-
-	const card = document.createElement("a");
-	card.href = imageFile;
-
-	const thumbnail = document.createElement("img");
-	thumbnail.setAttribute("data-src", imageFile);
-	thumbnail.width = 642 * 0.8;
-	thumbnail.height = 390 * 0.8;
-	thumbnail.alt = themeName;
-	thumbnail.className = "lazyload";
-
-	const name = document.createElement("h3");
-	name.textContent = themeName;
-
-	card.append(thumbnail, name);
-
-	const item = document.createElement("li");
-	item.appendChild(card);
-	document.getElementById("thumblist").appendChild(item);
-
-	return card;
-}
-
-function hideStuff(distros, instructions) {
-	distros.forEach(d => d.classList.remove("active"));
-	instructions.forEach(i => i.style.display = "none");
-}
